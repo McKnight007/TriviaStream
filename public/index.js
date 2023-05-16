c17fffa6177691ff4e754e92183165d1d119afbc
McKnight007 <114586694+McKnight007@users.noreply.github.com>
2023-05-17 00:39:49 +0400

    Add files via upload
    initial commit

---
+++ public/index.js
@@ -0,0 +1,212 @@
+const quizQuestions = [
+    {
+        question: "What is the capital of France?",
+        answers: {
+            a: "New York",
+            b: "Delhi",
+            c: "Paris",
+            d: "Rome",
+        },
+        correctAnswer: "c",
+    },
+    {
+        question: "Who discovered penicillin?",
+        answers: {
+            a: "Marie Curie",
+            b: "Alexander Fleming",
+            c: "Albert Einstein",
+            d: "Isaac Newton",
+        },
+        correctAnswer: "b",
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        answers: {
+            a: "Mars",
+            b: "Venus",
+            c: "Jupiter",
+            d: "Neptune",
+        },
+        correctAnswer: "a",
+    },
+    {
+        question: "What is the chemical symbol for gold?",
+        answers: {
+            a: "Ag",
+            b: "Au",
+            c: "Hg",
+            d: "Cu",
+        },
+        correctAnswer: "b",
+    },
+    {
+        question: "Who painted the Mona Lisa?",
+        answers: {
+            a: "Vincent van Gogh",
+            b: "Pablo Picasso",
+            c: "Leonardo da Vinci",
+            d: "Michelangelo",
+        },
+        correctAnswer: "c",
+    },
+    {
+        question: "What is the tallest mountain in the world?",
+        answers: {
+            a: "Mount Kilimanjaro",
+            b: "Mount Everest",
+            c: "Mount Fuji",
+            d: "Mount McKinley",
+        },
+        correctAnswer: "b",
+    },
+    {
+        question: "Which country is known for inventing tea?",
+        answers: {
+            a: "China",
+            b: "India",
+            c: "Japan",
+            d: "England",
+        },
+        correctAnswer: "a",
+    },
+    {
+        question: "Who wrote the play 'Romeo and Juliet'?",
+        answers: {
+            a: "William Shakespeare",
+            b: "Charles Dickens",
+            c: "Jane Austen",
+            d: "Mark Twain",
+        },
+        correctAnswer: "a",
+    },
+    {
+        question: "What is the largest organ in the human body?",
+        answers: {
+            a: "Liver",
+            b: "Brain",
+            c: "Heart",
+            d: "Skin",
+        },
+        correctAnswer: "d",
+    },
+    {
+        question: "Which animal is known as the 'King of the Jungle'?",
+        answers: {
+            a: "Tiger",
+            b: "Lion",
+            c: "Elephant",
+            d: "Giraffe",
+        },
+        correctAnswer: "b",
+    },
+    {
+        question: "Who was the first person to step on the moon?",
+        answers: {
+            a: "Buzz Aldrin",
+            b: "Neil Armstrong",
+            c: "Yuri Gagarin",
+            d: "Alan Shepard",
+        },
+        correctAnswer: "b",
+    },
+    {
+        question: "Which planet is known as the Blue Planet?",
+        answers: {
+            a: "Earth",
+            b: "Mars",
+            c: "Neptune",
+            d: "Saturn",
+        },
+        correctAnswer: "a",
+    },
+    {
+        question: "Who wrote the famous novel 'Prideand Prejudice'?",
+        answers: {
+            a: "Jane Austen",
+            b: "Emily Bronte",
+            c: "Charlotte Bronte",
+            d: "Virginia Woolf",
+        },
+        correctAnswer: "a",
+    },
+    {
+        question: "Which city is the capital of Australia?",
+        answers: {
+            a: "Melbourne",
+            b: "Sydney",
+            c: "Canberra",
+            d: "Brisbane",
+        },
+        correctAnswer: "c",
+    },
+    {
+        question: "What is the chemical symbol for sodium?",
+        answers: {
+            a: "Sa",
+            b: "So",
+            c: "Na",
+            d: "No",
+        },
+        correctAnswer: "c",
+    },
+    {
+        question: "Who is the artist of the famous painting 'The Starry Night'?",
+        answers: {
+            a: "Pablo Picasso",
+            b: "Salvador Dali",
+            c: "Claude Monet",
+            d: "Vincent van Gogh",
+        },
+        correctAnswer: "d",
+    },
+    {
+        question: "What is the largest ocean in the world?",
+        answers: {
+            a: "Atlantic Ocean",
+            b: "Indian Ocean",
+            c: "Arctic Ocean",
+            d: "Pacific Ocean",
+        },
+        correctAnswer: "d",
+    },
+    {
+        question: "Who is the Greek god of thunder?",
+        answers: {
+            a: "Zeus",
+            b: "Hades",
+            c: "Apollo",
+            d: "Poseidon",
+        },
+        correctAnswer: "a",
+    },
+    {
+        question: "What is the primary language spoken in Brazil?",
+        answers: {
+            a: "English",
+            b: "Spanish",
+            c: "Portuguese",
+            d: "Italian",
+        },
+        correctAnswer: "c",
+    },
+    {
+        question: "Which animal is a symbol of wisdom?",
+        answers: {
+            a: "Owl",
+            b: "Eagle",
+            c: "Lion",
+            d: "Snake",
+        },
+        correctAnswer: "a",
+    },
+    {
+        question: "Who is the author of the 'Harry Potter' book series?",
+        answers: {
+            a: "J.R.R. Tolkien",
+            b: "J.K. Rowling",
+            c: "George R.R. Martin",
+            d: "Stephen King",
+        },
+        correctAnswer: "b",
+    },
+];
